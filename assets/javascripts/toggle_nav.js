bfeb9e6c4327df3bc95424619e876d8869e4e3e7
Jeremias Dombrowsky <jerry@badabam.de>
2014-03-22 14:51:18 +0100

    Add toggle for xs nav

---
+++ assets/javascripts/toggle_nav.js
@@ -0,0 +1,11 @@
+(function(){
+  "use strict";
+
+  $("[data-toggle-nav]").on("click", function () {
+    var el = $(this),
+          target = $(el.attr("data-toggle-nav"));
+
+          target.toggleClass("hidden-xs");
+  });
+
+}());
