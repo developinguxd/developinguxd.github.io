6165b174125c94dbe9d7d5f816c28ab5458c6ea4
Jeremias Dombrowsky <jerry@badabam.de>
2014-05-18 12:19:51 +0200

    Fix styles for mobile nav

--- _assets/javascripts/_main-nav.js
+++ _assets/javascripts/_main-nav.js
@@ -1,19 +1,21 @@
 $(function () {
   "use strict";
 
+  var nav      = $("#main-nav"),
+      navLinks = nav.find(".nav-link");
+
   function onNavLinkClick (event) {
+    debugger;
     if ($(event.currentTarget).hasClass("nav-link--active")) {
       event.preventDefault();
     }
-
+    debugger;
     navLinks.each(function () {
       $(this).toggleClass("nav-link--active", this === event.currentTarget);
     });
     nav.toggleClass("nav--active");
   }
 
-  var nav      = $("#main-nav"),
-      navLinks = nav.find(".nav-link");
 
   navLinks.on("click", onNavLinkClick);
 });
