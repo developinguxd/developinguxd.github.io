3fd6364392641a1b1916697776a612de5bd81c21
Jeremias Dombrowsky <jerry@badabam.de>
2014-05-24 23:01:52 +0200

    Update mailchimp js

--- _assets/javascripts/_mailchimp.js
+++ _assets/javascripts/_mailchimp.js
@@ -3,6 +3,7 @@
 
   $(function () {
     var form    = $("#mc-embedded-subscribe-form"),
+        input   = form.find("input[name=EMAIL]"),
         action  = form.attr("action"),
         success = form.find(".js-done-response"),
         error   = form.find(".js-fail-response"),
