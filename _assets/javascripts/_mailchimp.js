0edd623a97619dd0e5798dbc7e20ef78fba7100b
Jeremias Dombrowsky <jerry@badabam.de>
2014-05-25 00:11:15 +0200

    Update form

--- _assets/javascripts/_mailchimp.js
+++ _assets/javascripts/_mailchimp.js
@@ -1,4 +1,31 @@
 (function($, window, undefined) {
   "use strict";
-  $("#mailchimp-form").ajaxChimp();
+
+  function onDone (result) {
+    if (result === "success") {
+      $("#mailchimp-form").find("button, input").remove();
+    }
+  }
+
+  function addSubmitHandler () {
+    $("#mailchimp-form").ajaxChimp({
+      callback: onDone
+    });
+  }
+
+  function removeSubmitHandler () {
+    $("#mailchimp-form").off();
+  }
+
+
+  $(document).on("ajaxSend", function () {
+    removeSubmitHandler();
+  });
+
+  $(document).on("ajaxDone", function () {
+    addSubmitHandler();
+  });
+
+  addSubmitHandler();
+
 }(jQuery, this));
