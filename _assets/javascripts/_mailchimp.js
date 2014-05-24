d3f775a558631f4b026078f136c89571f69eb1f3
Jeremias Dombrowsky <jerry@badabam.de>
2014-05-25 01:03:04 +0200

    Cleanup code

--- _assets/javascripts/_mailchimp.js
+++ _assets/javascripts/_mailchimp.js
@@ -1,23 +1,6 @@
 (function($, window, undefined) {
   "use strict";
 
-  function addSubmitHandler () {
-    $("#mailchimp-form").find("button, input").prop("disabled", true);
-  }
-
-  function removeSubmitHandler () {
-    $("#mailchimp-form").find("button, input").prop("disabled", false);
-  }
-
-
-  $(document).on("ajaxSend", function () {
-    removeSubmitHandler();
-  });
-
-  $(document).on("ajaxDone", function () {
-    addSubmitHandler();
-  });
-
   $("#mailchimp-form").ajaxChimp();
 
 }(jQuery, this));
