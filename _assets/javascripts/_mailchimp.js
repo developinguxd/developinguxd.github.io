f002631c9cc177e863cad922edb53e96b9e26483
Jeremias Dombrowsky <jerry@badabam.de>
2014-05-24 20:13:37 +0200

    Start with mailchimp ajax solution

---
+++ _assets/javascripts/_mailchimp.js
@@ -0,0 +1,29 @@
+(function($, window, undefined) {
+  "use strict";
+
+  $(function () {
+    var form    = $("#mc-embedded-subscribe-form"),
+        action  = form.attr("action"),
+        success = form.find(".js-done-response"),
+        error   = form.find(".js-fail-response"),
+        request;
+
+    function reset () {
+      error.add(success).addClass("hidden").text("");
+    }
+
+    form.on("submit", function (event) {
+      event.preventDefault();
+      request = $.post(action);
+      request.done(function onRequestDone (result) {
+        reset();
+        result === "success" && success.text("Well done! See you soon!");
+      });
+      request.fail(function onRequestFail (error) {
+        reset();
+        success.text("O-oh: " + error.message);
+      });
+
+    });
+  });
+}(jQuery, this));
