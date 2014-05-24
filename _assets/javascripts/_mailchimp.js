0cdb4cb04c4d259885343291a2cfe5b1420c932c
Jeremias Dombrowsky <jerry@badabam.de>
2014-05-24 23:47:29 +0200

    Move to jQuery plugin for mailchimp

--- _assets/javascripts/_mailchimp.js
+++ _assets/javascripts/_mailchimp.js
@@ -1,38 +1,4 @@
 (function($, window, undefined) {
   "use strict";
-
-  $(function () {
-    var form    = $("#mc-embedded-subscribe-form"),
-        action  = form.attr("action"),
-        method  = form.attr("method"),
-        success = form.find(".js-done-response"),
-        error   = form.find(".js-fail-response"),
-        request;
-
-    function reset () {
-      error.add(success).addClass("hidden").text("");
-    }
-
-    form.on("submit", function (event) {
-      event.preventDefault();
-      request = $.ajax({
-        type        : method,
-        url         : action,
-        data        : form.serialize(),
-        cache       : false,
-        dataType    : "json",
-        contentType : "application/json; charset=utf-8"
-    });
-
-      request.done(function onRequestDone (result) {
-        reset();
-        result === "success" && success.text("Well done! See you soon!");
-      });
-      request.fail(function onRequestFail (error) {
-        reset();
-        success.text("O-oh: " + error.message);
-      });
-
-    });
-  });
+  $("#mailchimp-form").ajaxChimp();
 }(jQuery, this));
