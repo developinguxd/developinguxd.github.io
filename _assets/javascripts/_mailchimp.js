c7f7a7fd9f2965fb910b025a76cb45a7d0d7259d
Jeremias Dombrowsky <jerry@badabam.de>
2014-05-25 01:31:56 +0200

    Finish ajax newsletter form

--- _assets/javascripts/_mailchimp.js
+++ _assets/javascripts/_mailchimp.js
@@ -1,6 +1,21 @@
 (function($, window, undefined) {
   "use strict";
 
-  $("#mailchimp-form").ajaxChimp();
+  var form       = $("#mailchimp-form"),
+      input      = form.find("input[type=email]"),
+      button     = form.find("button[type=submit]"),
+      buttonText = button.text();
+
+  form.ajaxChimp();
+
+  $(document).on("ajaxStart", function () {
+    input.add(button).prop("disabled", true);
+    button.text("Sending ...").addClass("sending");
+  });
+
+  $(document).on("ajaxComplete", function () {
+    input.add(button).prop("disabled", false);
+    button.text(buttonText).removeClass("sending");
+  });
 
 }(jQuery, this));
